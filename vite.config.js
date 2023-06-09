db4eec464a80726c1c87d31fa63c332c8d6393f1
Mariusz Trocewicz <throcu@gmail.com>
2023-06-09 16:46:53 +0200

    Added base key in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-03-image-finder',
+  base: '/goit-react-hw-04-images',
 });
