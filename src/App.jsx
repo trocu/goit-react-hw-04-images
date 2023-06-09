7182e5330aa9b6e7f987ffa3bd06fae242b73c22
Mariusz Trocewicz <throcu@gmail.com>
2023-06-10 01:51:54 +0200

    Changed the App class component to a functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import fetchPictures from './utils/fetchPictures';
 import Searchbar from './components/searchbar/Searchbar';
 import { ImageGallery } from './components/imageGallery/ImageGallery';
@@ -6,96 +6,137 @@ import { Button } from './components/button/Button';
 import Modal from './components/modal/Modal';
 import { Loader } from './components/loader/Loader';
 
-export default class App extends Component {
-  state = {
-    gallery: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    error: null,
-    totalHits: 0,
-    hitsCounter: 0,
-    isModalVisible: false,
-    picture: '',
-    alt: '',
+export const App = () => {
+  const [gallery, setGallery] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+  const [hitsCounter, setHitsCounter] = useState(0);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [picture, setPicture] = useState('');
+  const [alt, setAlt] = useState('');
+
+  // useEffect(() => {
+  //   if (query.length > 0) {
+  //     handleQuery();
+  //   }
+  // }, [query]);
+
+  // useEffect(() => {
+  //   if (page > 1) {
+  //     handleLoadMoreFetch();
+  //   }
+  // }, [page]);
+
+  const handleSubmit = query => {
+    setQuery(query);
+    setGallery([]);
+    setPage(1);
+    shouldFetchData.current = true;
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.handleQuery();
-    }
-    if (prevState.query === this.state.query && prevState.page !== this.state.page) {
-      this.handleLoadMoreFetch();
-    }
-  }
+  const picturesRef = useRef(null);
+  const shouldFetchData = useRef(false);
 
-  handleSubmit = query => {
-    this.setState({ query, gallery: [], page: 1 });
-  };
+  useEffect(() => {
+    const handleQuery = async () => {
+      setIsLoading(true);
+      try {
+        const pictures = await fetchPictures.restApi(query, page);
+        picturesRef.current = pictures;
+        const { hits, totalHits } = picturesRef.current;
+        setGallery(hits);
+        setTotalHits(totalHits);
+        setHitsCounter(hits.length);
+        shouldFetchData.current = false;
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (shouldFetchData.current && query.length > 0) {
+      handleQuery();
+    }
+  }, [query, page, shouldFetchData]);
 
-  handleQuery = async () => {
-    const { query, page } = this.state;
-    this.setState({ isLoading: true });
-    try {
+  // const handleQuery = async () => {
+  //   setIsLoading(true);
+  //   try {
+  //     const pictures = await fetchPictures.restApi(query, page);
+  //     setGallery(pictures.hits);
+  //     setTotalHits(pictures.totalHits);
+  //     setHitsCounter(pictures.hits.length);
+  //   } catch (error) {
+  //     setError(error);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  const loadMoreRef = useRef(null);
+  const shouldLoadMore = useRef(false);
+
+  useEffect(() => {
+    const handleLoadMoreFetch = async () => {
+      setIsLoading(true);
       const pictures = await fetchPictures.restApi(query, page);
-      this.setState({
-        gallery: pictures.hits,
-        totalHits: pictures.totalHits,
-        hitsCounter: pictures.hits.length,
-      });
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
+      loadMoreRef.current = pictures;
+      const { hits } = loadMoreRef.current;
+      setGallery([...gallery, ...hits]);
+      setIsLoading(false);
+      setHitsCounter(hitsCounter + hits.length);
+      shouldLoadMore.current = false;
+    };
+    if (shouldLoadMore.current) {
+      handleLoadMoreFetch();
     }
+  }, [gallery, hitsCounter, query, page, shouldLoadMore]);
+
+  // const handleLoadMoreFetch = async () => {
+  //   setIsLoading(true);
+  //   const pictures = await fetchPictures.restApi(query, page);
+  //   setGallery([...gallery, ...pictures.hits]);
+  //   setIsLoading(false);
+  //   setHitsCounter(hitsCounter + pictures.hits.length);
+  // };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    shouldLoadMore.current = true;
   };
 
-  handleLoadMoreFetch = async () => {
-    const { query, page } = this.state;
-    this.setState({ isLoading: true });
-    const pictures = await fetchPictures.restApi(query, page);
-    this.setState(prevState => ({
-      gallery: [...prevState.gallery, ...pictures.hits],
-      isLoading: false,
-      hitsCounter: prevState.hitsCounter + pictures.hits.length,
-    }));
+  const showModal = (picture, alt) => {
+    setIsModalVisible(true);
+    setPicture(picture);
+    setAlt(alt);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const closeModal = () => {
+    setIsModalVisible(false);
   };
 
-  showModal = (picture, alt) => {
-    this.setState({ isModalVisible: true, picture, alt });
-  };
-
-  closeModal = () => {
-    this.setState({ isModalVisible: false });
-  };
-
-  render() {
-    const { isLoading, gallery, error, totalHits, hitsCounter, isModalVisible, picture, alt } =
-      this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {error && <p>Whoops, something went wrong: {error.message}</p>}
-        {gallery.length > 0 && (
-          <ImageGallery
-            gallery={gallery}
-            onClickCallback={this.showModal}
-          />
-        )}
-        {isLoading && <Loader />}
-        {gallery.length > 0 && totalHits > hitsCounter && <Button loadMore={this.handleLoadMore} />}
-        {isModalVisible && (
-          <Modal
-            onClick={this.closeModal}
-            picture={picture}
-            alt={alt}
-          />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {error && <p>Whoops, something went wrong: {error.message}</p>}
+      {gallery.length > 0 && (
+        <ImageGallery
+          gallery={gallery}
+          onClickCallback={showModal}
+        />
+      )}
+      {isLoading && <Loader />}
+      {gallery.length > 0 && totalHits > hitsCounter && <Button loadMore={handleLoadMore} />}
+      {isModalVisible && (
+        <Modal
+          onClick={closeModal}
+          picture={picture}
+          alt={alt}
+        />
+      )}
+    </>
+  );
+};
